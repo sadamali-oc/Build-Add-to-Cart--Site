fbdd54d4541269e5a8a3b4cbe172aaa1b61ec428
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-06-10 11:40:48 +0530

    console.log

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const inputFieldEl= document.getElementById("input-field")
+const  addButtonEl=document.getElementById("add-button")
+
+addButtonEl.addEventListener("click",function (){
+    console.log("Prodcut is added")
+})
+
